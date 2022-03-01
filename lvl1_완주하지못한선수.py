716f4e37cdd834817eef7d30be985a813b173faa
aseprite <dmd5760@gmail.com>
2022-03-01 23:43:11 +0900

    22/03/01

---
+++ lvl1_완주하지못한선수.py
@@ -0,0 +1,7 @@
+def solution(participant, completion):
+    answer = []
+    for i,l in zip(participant,completion):
+        answer.append(i)
+        if 
+        answer.remove(i)
+    return "".join(answer)
